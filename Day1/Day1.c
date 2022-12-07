feaef3b6bdfc8f003b734d861c68bd47391997d2
FoxClock <foxwellhayden@gmail.comclear>
2022-12-08 10:50:43 +1100

    Added stuff

--- Day1/Day1.c
+++ Day1/Day1.c
@@ -1,3 +1,18 @@
--e // AUTHOR:	Hayden Foxwell
--e // DATE:	08-12-2022
--e // PURPOSE:	Day 1 -
+// AUTHOR:	Hayden Foxwell
+// DATE:	08-12-2022
+// PURPOSE:	Day 1 -
+
+// Imports
+
+
+// Defines
+
+
+/* Main function */
+
+int main() {
+
+
+
+    return 1;
+}
